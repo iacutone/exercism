45dfe809c8aa4f306fbcef94bc09df464231be23
Eric Iacutone <eric.iacutone@gmail.com>
2016-11-21 19:55:18 -0500

    Complete bob exercise

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,16 @@
 defmodule Bob do
   def hey(input) do
-    cond do
-        true -> raise "Your implementation goes here"
+    string = String.strip(input)
 
+    cond do
+      String.length(string) == 0 ->
+        "Fine. Be that way!"
+      String.ends_with?(string, "?") ->
+        "Sure."
+      String.upcase(string) == string and String.match?(string, ~r/\p{L}/)->
+        "Whoa, chill out!"
+      true ->
+        "Whatever."
     end
   end
 end
