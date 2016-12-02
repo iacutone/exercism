3103d47ba65167f9a449adfb0baa164508df2f9b
Eric Iacutone <eric.iacutone@gmail.com>
2016-12-02 13:06:45 -0500

    Complete word count exercism

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,11 +1,8 @@
 defmodule Words do
-  @doc """
-  Count the number of words in the sentence.
-
-  Words are compared case-insensitively.
-  """
-  @spec count(String.t) :: map
-  def count(sentence) do
-
+  def count(text) do
+    text
+    |> String.downcase
+    |> String.split(~r/[^[:alnum:]-]/u, trim: true)
+    |> Enum.reduce(%{}, fn word, acc -> Map.update(acc, word, 1, &(&1 + 1)) end)
   end
 end
